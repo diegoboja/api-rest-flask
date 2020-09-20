3e5ca2851eb81a8825b05badeeb561c21b874c2a
diegoboja <diegoboja@gmail.com>
2020-09-19 21:13:39 -0300

    simulo una base de datos de productos en products.py

---
+++ products.py
@@ -0,0 +1,6 @@
+products = [
+    {'name':'notebook','price':1000,'quantity':4},
+    {'name':'mouse','price':20,'quantity':23},
+    {'name':'teclado','price':300,'quantity':13},
+    {'name':'cargador','price':500,'quantity':2}
+]
