bdeb105482f779823fa069372f3de061690613c1
diegoboja <diegoboja@gmail.com>
2020-09-23 17:58:41 -0300

    POST and PUT routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__) ## Creo mi aplicacion
 
@@ -17,8 +17,29 @@ def get_product(product_name):
     product_found = [product for product in products if product['name'] == product_name]
     if len(product_found) > 0 :
         return jsonify(product_found[0])
-    else:
-        return jsonify({"message":"product unavailable"})
+    return jsonify({"message":"product unavailable"})
 
+@app.route('/products', methods = ['POST'])
+def addProduct():
+    new_product = {
+        'name' : request.json['name'],
+        'price': request.json['price'],
+        'quantity': request.json['quantity']
+    }
+    products.append(new_product)
+    return jsonify({'message': "Product added", 'products':products})
+
+@app.route('/products/<product_name>', methods = ['PUT'])
+def editProduct(product_name):
+    productFound = [product for product in products if product['name'] == product_name]
+    if (len(productFound) > 0) :
+        productFound[0]['name'] = request.json['name']
+        productFound[0]['price'] = request.json['price']
+        productFound[0]['quantity'] = request.json['quantity']
+        return jsonify({
+            'message': 'Product updated',
+            'product updated':productFound[0]
+        })
+    return jsonify({"message":"product unavailable"})
 if __name__ == "__main__":  ## Inicio mi aplicacion y defino el puerto para el servidor
     app.run(debug=True, port=2315)
